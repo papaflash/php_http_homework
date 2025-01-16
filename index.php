77b749c356a382332ebf84a6f20b374728d2f83b
Alex Shagov <shagov25@gmail.com>
2025-01-16 19:36:31 +0300

    add dockerfile

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 declare(strict_types=1);
 session_start();
+
 /*
 if(isset($_SESSION['username'])) {
     header('Location: web/index.html?username='. $_SESSION['username']);
 }
 */
 //include('web/index.html');
-include("dadata.php");
+include("test.php");
--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require 'vendor/autoload.php';
+
 $token = "833f50904ce0c04b16be7b367ebc56c3179f74f6";
 $secret = "5d56c667e6562e44f3c9a98a446b1c2bbb95f1f6";
 
