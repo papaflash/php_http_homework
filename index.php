6e15e7530cb1dd071b3170548de081704d43f156
Alex Shagov <shagov25@gmail.com>
2025-01-16 20:07:13 +0300

    add dadata.php. Remove test.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 session_start();
-
+require 'vendor/autoload.php';
 /*
 if(isset($_SESSION['username'])) {
     header('Location: web/index.html?username='. $_SESSION['username']);
 }
 */
 //include('web/index.html');
-include("test.php");
+include('dadata.php');
